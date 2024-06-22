9429f0af9ce6d42febd83a445acc698eeacd8a05
Jake Rosoman <jkroso@gmail.com>
2024-06-22 11:27:01 +1000

    refactor to make subtyping easier

--- ReadBuffer.jl
+++ ReadBuffer.jl
@@ -1,50 +1,49 @@
-"An wrapper that adds byte IO to any IO type"
-mutable struct ReadBuffer <: IO
+@use "." AbstractBuffer @abstract @mutable
+
+@abstract struct AbstractReadBuffer <: AbstractBuffer
   io::IO
-  buffer::Vector{UInt8}
-  i::Int
-  mark::Int
 end
 
-ReadBuffer(io) = ReadBuffer(io, UInt8[], 0, -1)
+"An wrapper that adds byte IO to any IO type"
+@mutable ReadBuffer <: AbstractReadBuffer
 
-Base.write(io::ReadBuffer, b::UInt8) = write(io.io, b)
-Base.read(io::ReadBuffer, ::Type{UInt8}) = begin
-  io.i < length(io.buffer) || pull!(io)
-  @inbounds io.buffer[io.i+=1]
+Base.write(io::AbstractReadBuffer, b::UInt8) = write(io.io, b)
+Base.read(io::AbstractReadBuffer, ::Type{UInt8}) = begin
+  io.i < length(io.data) || pull!(io)
+  @inbounds io.data[io.i+=1]
 end
 
 pull!(io::ReadBuffer) = begin
   bytes = eof(io.io) || readavailable(io.io)
   @assert bytes != true "Attempted to read from an empty IO"
   if ismarked(io)
-    append!(io.buffer, bytes)
+    append!(io.data, bytes)
   else
-    io.buffer = bytes
+    io.data = bytes
     io.i = 0
   end
 end
 
-Base.readavailable(io::ReadBuffer) = begin
-  if io.i < length(io.buffer)
-    bytes = @view io.buffer[io.i+1:end]
-    io.i = length(io.buffer)
+Base.readavailable(io::AbstractReadBuffer) = begin
+  if io.i < length(io.data)
+    bytes = @view io.data[io.i+1:end]
+    io.i = length(io.data)
     bytes
   else
     readavailable(io.io)
   end
 end
 
-Base.read(io::ReadBuffer, n::Integer) = begin
-  rem = length(io.buffer) - io.i
+Base.read(io::AbstractReadBuffer, n::Integer) = begin
+  rem = length(io.data) - io.i
   if rem >= n
-    bytes = @view io.buffer[io.i+1:io.i+n]
+    bytes = @view io.data[io.i+1:io.i+n]
     io.i += n
     bytes
   else
     out = IOBuffer(maxsize=n)
-    len = length(io.buffer)
-    n -= write(out, @view io.buffer[io.i+1:len])
+    len = length(io.data)
+    n -= write(out, @view io.data[io.i+1:len])
     io.i = len
     while n > 0
       n -= write(out, read(io.io, n))
@@ -53,41 +52,39 @@ Base.read(io::ReadBuffer, n::Integer) = begin
   end
 end
 
-Base.read(io::ReadBuffer) = begin
-  rem = length(io.buffer) - io.i
+Base.read(io::AbstractReadBuffer) = begin
+  rem = length(io.data) - io.i
   rem <= 0 && return eof(io.io) ? UInt8[] : read(io.io)
-  eof(io.io) || append!(io.buffer, read(io.io))
-  len = length(io.buffer)
-  bytes = @view io.buffer[io.i+1:len]
+  eof(io.io) || append!(io.data, read(io.io))
+  len = length(io.data)
+  bytes = @view io.data[io.i+1:len]
   io.i = len
   bytes
 end
 
-Base.isopen(io::ReadBuffer) = isopen(io.io)
-Base.isreadable(io::ReadBuffer) = isreadable(io.io)
-Base.iswritable(io::ReadBuffer) = iswritable(io.io)
-Base.close(io::ReadBuffer) = close(io.io)
-Base.eof(io::ReadBuffer) = io.i == length(io.buffer) && eof(io.io)
-Base.bytesavailable(io::ReadBuffer) = begin
-  if io.i < length(io.buffer)
-    length(io.buffer) - io.i
+Base.isopen(io::AbstractReadBuffer) = isopen(io.io)
+Base.isreadable(io::AbstractReadBuffer) = isreadable(io.io)
+Base.iswritable(io::AbstractReadBuffer) = iswritable(io.io)
+Base.close(io::AbstractReadBuffer) = close(io.io)
+Base.eof(io::AbstractReadBuffer) = io.i == length(io.data) && eof(io.io)
+Base.bytesavailable(io::AbstractReadBuffer) = begin
+  if io.i < length(io.data)
+    length(io.data) - io.i
   else
     bytesavailable(io.io)
   end
 end
 
-Base.position(io::ReadBuffer) = io.i
-Base.seek(io::ReadBuffer, pos::Integer) = begin
+Base.seek(io::AbstractReadBuffer, pos::Integer) = begin
   @assert pos >= 0 "Can't seek back that far"
-  while pos >= length(io.buffer)
+  while pos >= length(io.data)
     bytes = eof(io.io) || readavailable(io.io)
     @assert bytes != true "Can't seek forward that far"
-    append!(io.buffer, bytes)
+    append!(io.data, bytes)
   end
   io.i = pos
   io
 end
-Base.skip(io::ReadBuffer, offset::Integer) = seek(io, position(io) + offset)
 
 "Wraps an input in a Buffer so that consumers can use the full IO interface"
 buffer(io::T) where T<:IO = hasmethod(position, Tuple{T}) ? io : ReadBuffer(io)
--- main.jl
+++ main.jl
@@ -1,74 +1,78 @@
-abstract type AbstractBuffer <: IO end
+@use "github.com/jkroso/Prospects.jl" @abstract @mutable
 
-mutable struct Buffer <: AbstractBuffer
-  ptr::Int
-  open::Bool
-  onwrite::Condition
-  mark::Int
-  data::Vector{UInt8}
+@abstract struct AbstractBuffer <: IO
+  i::Int=0
+  mark::Int=-1
+  data::Vector{UInt8}=UInt8[]
 end
 
-Buffer() = Buffer(UInt8[])
-Buffer(buf::Vector{UInt8}) = Buffer(0, true, Condition(), -1, buf)
+@abstract struct AsyncBuffer <: AbstractBuffer
+  open::Bool=true
+  onwrite::Condition=Condition()
+end
 
-Base.write(io::AbstractBuffer, b::UInt8) = begin
+@mutable Buffer <: AsyncBuffer
+
+Buffer(buf::Vector{UInt8}) = Buffer(true, Condition(), 0, -1, buf)
+
+Base.write(io::AsyncBuffer, b::UInt8) = begin
   @assert io.open
   push!(io.data, b)
   notify(io.onwrite, false)
   1
 end
 
-Base.write(io::AbstractBuffer, b::Vector{UInt8}) = begin
+Base.write(io::AsyncBuffer, b::Vector{UInt8}) = begin
   @assert io.open
   append!(io.data, b)
   notify(io.onwrite, false)
   length(b)
 end
 
-Base.isopen(io::AbstractBuffer) = io.open
-Base.eof(io::Buffer) = bytesavailable(io) > 0 ? false : isopen(io) ? wait(io.onwrite) : true
-Base.close(io::AbstractBuffer) = (io.open = false; notify(io.onwrite, true); nothing)
-Base.bytesavailable(io::Buffer) = length(io.data) - io.ptr
+Base.isopen(io::AsyncBuffer) = io.open
+Base.eof(io::AsyncBuffer) = bytesavailable(io) > 0 ? false : isopen(io) ? wait(io.onwrite) : true
+Base.close(io::AsyncBuffer) = (io.open = false; notify(io.onwrite, true); nothing)
+Base.bytesavailable(io::AbstractBuffer) = length(io.data) - io.i
 
 Base.readavailable(io::AbstractBuffer) = begin
-  ptr = io.ptr
-  bytes = ptr > 0 ? @view(io.data[ptr+1:end]) : io.data
+  i = io.i
+  bytes = i > 0 ? @view(io.data[i+1:end]) : io.data
   ismarked(io) || clear!(io)
   bytes
 end
 
 clear!(io::AbstractBuffer) = begin
   io.data = UInt8[]
-  io.ptr = 0
+  io.i = 0
 end
 
-Base.read(io::AbstractBuffer) = begin
+Base.read(io::AsyncBuffer) = begin
   while isopen(io); wait(io.onwrite) end
-  bytes = @view io.data[io.ptr+1:end]
-  io.ptr = length(io.data)
+  bytes = @view io.data[io.i+1:end]
+  io.i = length(io.data)
   bytes
 end
 
-Base.read(io::AbstractBuffer, n::Integer) = begin
+Base.read(io::AsyncBuffer, n::Integer) = begin
   while isopen(io) && bytesavailable(io) < n; wait(io.onwrite) end
   @assert bytesavailable(io) >= n
-  ptr = io.ptr
-  io.ptr += n
-  @view io.data[ptr+1:io.ptr]
+  ptr = io.i
+  io.i += n
+  @view io.data[ptr+1:io.i]
 end
 
 Base.read(io::AbstractBuffer, ::Type{UInt8}) = begin
   @assert !eof(io)
-  @inbounds io.data[io.ptr+=1]
+  @inbounds io.data[io.i+=1]
 end
 
-Base.position(io::AbstractBuffer) = io.ptr
-Base.skip(io::AbstractBuffer, n::Integer) = seek(io, position(io) + n)
-Base.seek(io::AbstractBuffer, pos::Integer) = begin
+Base.position(io::AbstractBuffer) = io.i
+Base.skip(io::AbstractBuffer, n::Integer) = seek(io, io.i + n)
+Base.seek(io::AsyncBuffer, pos::Integer) = begin
   @assert pos >= 0 "Can't seek back that far"
   while !eof(io) && pos > length(io.data); wait(io.onwrite) end
   @assert pos <= length(io.data) "Can't seek forward that far"
-  io.ptr = pos
+  io.i = pos
   io
 end
 
